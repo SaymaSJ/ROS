23428d90ce9b62a3ea923e19260c48583c93dcb6
Sayma S Jhara <158565415+SaymaSJ@users.noreply.github.com>
2026-07-08 03:52:11 +0600

    Add files via upload

---
+++ 3 dof(dissertation)/cobot_description/launch/display.launch.py
@@ -0,0 +1,26 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('cobot_description')
+    urdf_path = os.path.join(pkg_path, 'urdf', 'cobot.urdf')
+    with open(urdf_path, 'r') as f:
+        robot_desc = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2'
+        ),
+    ])
